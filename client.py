6b89b674170d6069e8b0786f6dc5c9c2c155dd94
Blake Fasse <blakefasse@gmail.com>
2019-11-06 12:46:05 -0600

    Further progress on assignment

--- client.py
+++ client.py
@@ -1,20 +1,38 @@
-# Import socket module 
-import socket                
+import socket
+
+def menu():
+    print("**********Welcome to XYZ ATM********")
+    print("Press 1 for Deposit.")
+    print("Press 2 for Withdrawal")
+    print("Press 3 for Account Balance")
+    print("Press 4 to Quit")
+host = socket.gethostname()
+port = 64000
+s = socket.socket()
+
+s.connect((host, port))
+
+while True:
+    menu()
+    print("What would you like to do: ")
+    userchoice = input()
+    msg = str.encode(str(userchoice), 'utf-8')
+    s.send(msg)
+    print(s.recv(1024))
+    userchoice = input()
+    msg = str.encode(str(userchoice), 'utf-8')
+    s.send(msg)
+    print(s.recv(1024))
+s.close
+
+# s = socket.socket()          
   
-# Create a socket object 
-s = socket.socket()          
+# port = 12345                
   
-# Define the port on which you want to connect 
-port = 12345                
-  
-# connect to the server on local computer 
-temp =  0
-while temp < 5:
-    s.connect(('127.0.0.1', port)) 
-    
-    # receive data from the server 
-    print (s.recv(1024) )
-    # close the connection 
-    temp = temp + 1
+# temp =  0
+# while temp < 5:
+#     s.connect(('127.0.0.1', port)) 
+#     print (s.recv(1024) )
+#     temp = temp + 1
        
-s.close()
+# s.close()
--- server.py
+++ server.py
@@ -1,18 +1,77 @@
-import socket			 
+import socket, sys			 
+class Server():
+    def __init__(self):
+        self.account_balance = float(10000)
+        self.beginning_balance = self.account_balance
+        self.deposit_amount = float(0)
+        self.withdrawal_amount = float(0)
 
-s = socket.socket()		 
-print ("Socket successfully created")
+    def deposit (self, amount, conn):
+        self.account_balance += amount
+        self.deposit_amount += amount
+        print ("Deposit was $%.2f, current balance is $%.2f" % (amount, self.account_balance))
+        data = "Deposit was $%.2f, current balance is $%.2f" % (amount, self.account_balance)
+        conn.send(data.encode())
 
-port = 12345				
+    def withdrawal (self, amount, conn):
+        self.account_balance -= amount
+        self.withdrawal_amount += amount
+        print("Withdrawal was $%.2f, current balance is $%.2f" % (amount, self.account_balance))
+        data = "Withdrawal was $%.2f, current balance is $%.2f" % (amount, self.account_balance)
+        conn.send(data.encode())
 
-s.bind(('', port))		 
-print ("socket binded to %s" %(port) )
-s.listen(5)	 
-print ("socket is listening")
+    def print (self, conn):
+        print("Your current balance: $%.2f" % self.account_balance)
+        data = "Your current balance: $%.2f" % self.account_balance
+        conn.send(data.encode())
 
-while True: 
-    c, addr = s.accept()	 
-    print ('Got connection from', addr )
+def main():
+    host = socket.gethostname()
+    port = 64000
+    s = socket.socket()
+    s.bind((host, port))
 
-c.send('Thank you for connecting') 
-c.close() 
+    s.listen(5)
+    c, addr = s.accept()
+    print('Got connection from', addr)
+    account = Server()
+    while True:
+        userchoice = c.recv(1024).decode()
+        if userchoice == '1':
+            c.send("How much would you like to deposit today?".encode())
+            amount = float(c.recv(1024).decode())
+            account.deposit(amount, c)
+        elif userchoice == '2':
+            c.send("How much would you like to withdraw?".encode())
+            amount = float(c.recv(1024).decode())
+            account.withdrawal(amount, c)
+        elif userchoice == '3':
+            account.print(c)
+        elif userchoice == '4':
+            c.send("Thank You for banking with us!".encode())
+            sys.exit(1)
+        else:
+            print("Wrong Choice!")
+            c.send("Wrong Choice!".encode())
+    c.close()
+main()
+
+
+
+
+# s = socket.socket()		 
+# print ("Socket successfully created")
+
+# port = 12345				
+
+# s.bind(('', port))		 
+# print ("socket binded to %s" %(port) )
+# s.listen(5)	 
+# print ("socket is listening")
+
+# while True: 
+#     c, addr = s.accept()	 
+#     print ('Got connection from', addr )
+
+# c.send('Thank you for connecting') 
+# c.close() 
